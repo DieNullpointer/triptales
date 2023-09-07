9d19d6b74a54a9eb4abb027c3f05a7fcac234db3
SCH22538 <SCH22538@spengergasse.at>
2023-09-07 15:21:17 +0200

    chore: added roboto

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -3,7 +3,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div class="p-4">
+  <div class="p-4 font-roboto">
     <HelloWorld msg="Test Test" />
   </div>
 </template>
--- src/client/tailwind.config.js
+++ src/client/tailwind.config.js
@@ -5,6 +5,10 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['Roboto'],
+      'roboto': ['Roboto', 'sans-serif'],
+    },
     extend: {},
     colors: {
       ...colors
