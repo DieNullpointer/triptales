b3abe76236bc392357d49ebd629c6d6498560d49
Bastian Seidl <sei20375@spengergasse.at>
2023-09-14 13:27:44 +0200

    create DBContext

---
+++ src/TripTales.Application/Infrastructure/TripTalesContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripTales.Application.Infrastructure
+{
+    public class TripTalesContext : DbContext
+    {
+        public TripTalesContext(DbContextOptions opt) : base(opt)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Generic config for all entities
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                // ON DELETE RESTRICT instead of ON DELETE CASCADE
+                foreach (var key in entityType.GetForeignKeys())
+                    key.DeleteBehavior = DeleteBehavior.Restrict;
+
+                foreach (var prop in entityType.GetDeclaredProperties())
+                {
+                    // Define Guid as alternate key. The database can create a guid fou you.
+                    if (prop.Name == "Guid")
+                    {
+                        modelBuilder.Entity(entityType.ClrType).HasAlternateKey("Guid");
+                        prop.ValueGenerated = Microsoft.EntityFrameworkCore.Metadata.ValueGenerated.OnAdd;
+                    }
+                    // Default MaxLength of string Properties is 255.
+                    if (prop.ClrType == typeof(string) && prop.GetMaxLength() is null) prop.SetMaxLength(255);
+                    // Seconds with 3 fractional digits.
+                    if (prop.ClrType == typeof(DateTime)) prop.SetPrecision(3);
+                    if (prop.ClrType == typeof(DateTime?)) prop.SetPrecision(3);
+                }
+            }
+        }
+    }
+}
