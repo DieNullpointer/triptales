96152be4a000f089030174e1f680c74157011f85
Bastian Seidl <sei20375@spengergasse.at>
2023-09-15 10:44:08 +0200

    add password encryption

--- src/TripTales.Application/Model/User.cs
+++ src/TripTales.Application/Model/User.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,8 @@ namespace TripTales.Application.Model
         public int Id { get; set; }
         public Guid Guid { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string Salt { get; set; }
+        public string PasswordHash { get; set; }
         public string DisplayName { get; set; }
         public string RegistryName { get; set; }
         public List<User> Friends { get; } = new();
@@ -28,12 +30,53 @@ namespace TripTales.Application.Model
         public User(string email, string password, string displayName, string registryName)
         {
             Email = email;
-            Password = password;
+            SetPassword(password);
             DisplayName = displayName;
             RegistryName = registryName;
         }
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         protected User() { }
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+        // Hint for the compiler that we initialize some properties in this method.
+        [MemberNotNull(nameof(Salt), nameof(PasswordHash))]
+        public void SetPassword(string password)
+        {
+            Salt = GenerateRandomSalt();
+            PasswordHash = CalculateHash(password, Salt);
+        }
+        public bool CheckPassword(string password) => PasswordHash == CalculateHash(password, Salt);
+        /// <summary>
+        /// Generates a random number with the given length of bits.
+        /// </summary>
+        /// <param name="length">Default: 128 bits (16 Bytes)</param>
+        /// <returns>A base64 encoded string from the byte array.</returns>
+        private string GenerateRandomSalt(int length = 128)
+        {
+            byte[] salt = new byte[length / 8];
+            using (System.Security.Cryptography.RandomNumberGenerator rnd =
+                System.Security.Cryptography.RandomNumberGenerator.Create())
+            {
+                rnd.GetBytes(salt);
+            }
+            return Convert.ToBase64String(salt);
+        }
+
+        /// <summary>
+        /// Calculates a HMACSHA256 hash value with a given salt.
+        /// <returns>Base64 encoded hash.</returns>
+        private string CalculateHash(string password, string salt)
+        {
+            byte[] saltBytes = Convert.FromBase64String(salt);
+            byte[] passwordBytes = System.Text.Encoding.UTF8.GetBytes(password);
+
+            System.Security.Cryptography.HMACSHA256 myHash =
+                new System.Security.Cryptography.HMACSHA256(saltBytes);
+
+            byte[] hashedData = myHash.ComputeHash(passwordBytes);
+
+            // Das Bytearray wird als Hexstring zurückgegeben.
+            return Convert.ToBase64String(hashedData);
+        }
     }
 }
