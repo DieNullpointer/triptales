9c5fdad75cf9f906cee562ab8c242acab705a267
Bastian Seidl <sei20375@spengergasse.at>
2023-09-20 16:22:26 +0200

    add allowAnonymous

--- src/TripTales.Webapi/Controllers/UserController.cs
+++ src/TripTales.Webapi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,7 @@ namespace TripTales.Webapi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [AllowAnonymous]
     public class UserController : ControllerBase
     {
         private readonly TripTalesContext _db;
