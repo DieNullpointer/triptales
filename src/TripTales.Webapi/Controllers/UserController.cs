7cc2158570918d685c1b1cd5b11e0f0536520739
Maximilian Schwarz <sch22384@spengergasse.at>
2023-09-21 12:47:58 +0200

    backend login finished

--- src/TripTales.Webapi/Controllers/UserController.cs
+++ src/TripTales.Webapi/Controllers/UserController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using TripTales.Application.Dto;
 using TripTales.Application.Infrastructure;
 using TripTales.Application.Model;
+using TripTales.Application.Services;
 
 namespace TripTales.Webapi.Controllers
 {
@@ -19,11 +20,13 @@ namespace TripTales.Webapi.Controllers
     {
         private readonly TripTalesContext _db;
         private readonly IMapper _mapper;
+        private readonly AuthService _authService;
 
-        public UserController(TripTalesContext db, IMapper mapper)
+        public UserController(TripTalesContext db, IMapper mapper, AuthService authService)
         {
             _db = db;
             _mapper = mapper;
+            _authService = authService;
         }
 
         [HttpPost("register")]
@@ -46,6 +49,21 @@ namespace TripTales.Webapi.Controllers
         public IActionResult Get()
         {
             return Ok(_db.User.ToList());
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserCredentialsCmd credentials)
+        {
+            var jwt = await _authService.Login(credentials.registryName, credentials.password);
+            if (jwt is null) { return Unauthorized(); }
+            // Return the token so the client can save this to send a bearer token in the
+            // subsequent requests.
+            return Ok(new
+            {
+                Username = _authService.CurrentUser,
+                UserGuid = _authService.CurrentUserGuid,
+                Token = jwt
+            });
         }
     }
 }
---
+++ src/TripTales.Webapi/Services/AuthService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TripTales.Application.Infrastructure;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+namespace TripTales.Application.Services
+{
+    public class AuthService
+    {
+        private readonly IConfiguration _config;
+        private readonly TripTalesContext _db;
+
+        public AuthService(IConfiguration config, TripTalesContext db)
+        {
+            _config = config;
+            _db = db;
+        }
+
+        public string? CurrentUser { get; set; }
+        public Guid? CurrentUserGuid { get; set; }
+
+        public async Task<string?> Login(string username, string password)
+        {
+            var secret = Convert.FromBase64String(_config["Secret"]);
+            var lifetime = TimeSpan.FromHours(3);
+
+            var user = await _db.User.FirstOrDefaultAsync(a => a.RegistryName == username);
+            if(user is null) { return null; }
+            if(!user.CheckPassword(password)) { return null; }
+
+            CurrentUser = user.RegistryName;
+            CurrentUserGuid = user.Guid;
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.RegistryName.ToString()),
+                    new Claim("Guid", user.Guid.ToString())
+                }),
+                Expires = DateTime.UtcNow + lifetime,
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(secret),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
