2cbbb6a1ab460bbf9d5209ca24f35b9c6e4f8615
Louis Muhr <louismuhr8@gmail.com>
2023-09-21 11:45:11 +0200

    model classes done

--- src/TripTales.Application/Model/TripPost.cs
+++ src/TripTales.Application/Model/TripPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,9 @@ namespace TripTales.Application.Model
 {
     public class TripPost
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public Guid Guid { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
