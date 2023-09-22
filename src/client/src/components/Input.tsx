cecefcab12710078449f0b5ff5028070deb14ab1
SCH22538 <SCH22538@spengergasse.at>
2023-09-22 10:37:20 +0200

    feat: added input

--- src/client/src/components/Input.tsx
+++ src/client/src/components/Input.tsx
@@ -1,3 +1,38 @@
+import { Typography, Input } from "@material-tailwind/react";
+
 export interface Props {
-    
+    type?: string;
+    label: string;
+    bottomText?: string;
+    width?: string;
 }
+
+const CustomInput: React.FC<Props> = ({type = 'text', label, bottomText, width = "32rem"}) => {
+    
+    return (
+        <div style={{width: width}}>
+          <Input crossOrigin={null} className="!font-roboto" type={type} label={label} />
+          {bottomText && <Typography
+            variant="small"
+            color="gray"
+            className="mt-2 flex items-center gap-1 font-normal"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="-mt-px h-4 w-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+            {bottomText}
+          </Typography>}
+        </div>
+      );
+}
+
+export default CustomInput;
--- src/client/src/pages/index.tsx
+++ src/client/src/pages/index.tsx
@@ -1,11 +1,14 @@
 import Button from "@/components/Button";
+import Input from "@/components/Input";
 
 export default function Home() {
   return (
-    <main>
+    <main className="space-y-4">
       <h2 className='text-2xl'>HOMEPAGE</h2>
       <p className='leading-8 mb-6'>index.tsx editieren zum Bearbeiten.<br/>Hier die derzeitigen components (tailwind usw. eingebaut):</p>
       <Button>Button</Button>
+      <Input label="Username" width="15rem" />
+      <Input type="password" bottomText="Must include at least 8 characters" label="Password" />
     </main>
   )
 }
