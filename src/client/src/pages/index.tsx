bb91f845ac7c454abcb86b02fc3850f512135a77
SCH22538 <SCH22538@spengergasse.at>
2023-10-11 16:23:41 +0200

    feat: added middleware

--- src/client/src/pages/index.tsx
+++ src/client/src/pages/index.tsx
@@ -1,22 +1,36 @@
-import Button from "@/components/atoms/Button";
 import Container from "@/components/atoms/Container";
-import Input from "@/components/atoms/Input";
 import Spacing from "@/components/atoms/Spacing";
 import * as Text from "@/components/atoms/Text";
 import Image from "@/components/atoms/Image";
-import Link from "next/link";
+//import useSWR from "swr";
 
-import BigLogo from "@/resources/triptales_homepage_tight_transparent.png"
+import BigLogo from "@/resources/triptales_homepage_tight_transparent.png";
 
 export default function Home() {
+  /* const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(
+    "https://localhost:7174/api/User",
+    fetcher
+  );
+  console.log(data); */
+
   return (
     <div className="w-full">
       <Container center>
-        <Image src={BigLogo.src} alt="TripTales Logo" width={300} height={300} wrapper="flex justify-center items-center" />
-      <Spacing />
+        <Image
+          src={BigLogo.src}
+          alt="TripTales Logo"
+          width={300}
+          height={300}
+          wrapper="flex justify-center items-center"
+        />
+        <Spacing />
         <Text.Flowtext center gutter>
-          Explore the world of travelling with TripTales. Connect with other fellow travellers and explore the world together.<br />
-          Share memories, tips, images and routes throughout your journey, helping others to discover that location in the process.
+          Explore the world of travelling with TripTales. Connect with other
+          fellow travellers and explore the world together.
+          <br />
+          Share memories, tips, images and routes throughout your journey,
+          helping others to discover that location in the process.
         </Text.Flowtext>
         <Spacing line />
       </Container>
