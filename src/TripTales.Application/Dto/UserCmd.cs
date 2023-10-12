7cecca1676a8f61765d3e3c7fc62a4e61e1208d8
Maximilian Schwarz <sch22384@spengergasse.at>
2023-10-12 12:42:39 +0200

    feat: Finished ChangeUser in Backend

---
+++ src/TripTales.Application/Dto/UserCmd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripTales.Application.Dto
+{
+    public record UserCmd
+    (
+        Guid guid,
+        string Email,
+        string Password,
+        string DisplayName, 
+        string RegistryName
+    ); 
+}
