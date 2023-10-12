0886a8d4bc4f9f04d84ffaa0143a85c2e19860c3
SCH22538 <SCH22538@spengergasse.at>
2023-10-12 14:08:40 +0200

    style: finished beautiful user profile header

--- src/client/src/pages/user/[registryname].tsx
+++ src/client/src/pages/user/[registryname].tsx
@@ -3,6 +3,8 @@ import { getUser, getUserByRegistry } from "@/middleware/middleware";
 import { useRouter } from "next/router";
 
 import defaultBanner from "@/resources/default_bannerpic.jpg";
+import defaultPfp from "@/resources/default_profilepic.png";
+import { Flowtext, Heading, Subheading } from "@/components/atoms/Text";
 
 export default function User() {
   const router = useRouter();
@@ -11,7 +13,28 @@ export default function User() {
 
   return (
     <div className="">
-      <Image src={defaultBanner.src} alt="" className="w-full" wrapper="max-h-60 overflow-hidden items-center flex rounded-lg" />
+      <div className="relative">
+        <Image
+          src={defaultBanner.src}
+          alt=""
+          className="w-full"
+          wrapper="max-h-60 overflow-hidden items-center flex rounded-lg"
+        />
+        <Image
+          src={defaultPfp.src}
+          alt=""
+          height={110}
+          width={110}
+          className="rounded-full ring-8 ring-white"
+          wrapper="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"
+        />
+      </div>
+      <div className="mt-16">
+        <Subheading bold wide center>
+          {user.displayName}
+        </Subheading>
+        <Flowtext className="italic !text-slate-600" center>@{user.registryName}</Flowtext>
+      </div>
     </div>
   );
 }
