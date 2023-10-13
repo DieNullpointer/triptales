2dd2d1599b5ef20468dc7932e1482bdce9f491bc
Bastian Seidl <sei20375@spengergasse.at>
2023-10-13 14:13:34 +0200

    add Unit Tests

---
+++ test/TripTales.Application.Tests/UserRepositoryTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TripTales.Application.Dto;
+using TripTales.Application.Infrastructure;
+using TripTales.Application.Infrastructure.Repositories;
+using TripTales.Application.Model;
+
+namespace TripTales.Application.Tests
+{
+    [Collection("Sequential")]
+    public class UserRepositoryTest
+    {
+        public TripTalesContext GetDatabase()
+        {
+            var options = new DbContextOptionsBuilder()
+                .UseSqlite("DataSource=test.db")
+                .Options;
+            var db = new TripTalesContext(options);
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+            return db;
+        }
+
+        [Fact]
+        public async void DeleteSuccessTest()
+        {
+            // ARRange
+            var db = GetDatabase();
+            var repo = new UserRepository(db);
+
+            var user = new User("email@example.com", "password", "displayName", "registryName", "description");
+            await db.User.AddAsync(user);
+            await db.SaveChangesAsync();
+            db.ChangeTracker.Clear();
+
+            // ACT
+            await repo.Delete(user.Guid);
+
+            // ASSERT
+            Assert.True(db.User.Count() == 0);
+        }
+
+        [Fact]
+        public async void UpdateSuccessTest()
+        {
+            // ARRange
+            var db = GetDatabase();
+            var repo = new UserRepository(db);
+
+            var user = new User("email@example.com", "password", "displayName", "registryName", "description");
+            await db.User.AddAsync(user);
+            await db.SaveChangesAsync();
+            db.ChangeTracker.Clear();
+
+            // ACT
+            var user2 = new User("email@spengergasse.com", "1234", "test", "Name", "Beschreibung");
+            user2.Guid = user.Guid;
+            await repo.Update(user2);
+
+            // ASSERT
+            Assert.True(db.User.First().Guid == user.Guid && db.User.First().Email == user2.Email);
+        }
+    }
+}
