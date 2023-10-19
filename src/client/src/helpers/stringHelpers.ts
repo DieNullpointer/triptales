72e8514787bc44dd0d1ef8601b4e69727306a0ca
SCH22538 <SCH22538@spengergasse.at>
2023-10-19 14:24:16 +0200

    feat: added more post attributes in frontend

--- src/client/src/helpers/stringHelpers.ts
+++ src/client/src/helpers/stringHelpers.ts
@@ -2,3 +2,7 @@ export function cleanClasses(classes: string) {
     let classesArr = classes.replace(/(\s+|undefined)/g, ' ').trim();
     return classesArr;
 }
+export function formatDateEuropean(date: Date) {
+    var d = new Date(date);
+    return d.getDate() + "." + (d.getUTCMonth() + 1) + "." + d.getFullYear();
+}
