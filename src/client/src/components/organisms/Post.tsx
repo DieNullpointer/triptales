c0ba0fec34217821cbf279be619359b9284457d0
SCH22538 <SCH22538@spengergasse.at>
2023-10-19 12:24:33 +0200

    feat: fix date format

--- src/client/src/components/organisms/Post.tsx
+++ src/client/src/components/organisms/Post.tsx
@@ -9,29 +9,47 @@ import Loading from "../static/Loading";
 export interface Props {
   data: TripPost;
   small?: boolean;
-  loading?: boolean
+  loading?: boolean;
 }
 
 const Post: React.FC<Props> = ({ data, small, loading }) => {
-  return (
-    !loading ? <Container className="relative h-screen m-12" sectionMarker>
-    <div className="flex flex-row bg-white rounded items-center justify-between">
-      <div className="place-items-center flex flex-row">
-      <Image
-        src={defaultPfp.src}
-        alt=""
-        height={80}
-        width={80}
-        className="rounded-full ring-4 ring-white"
-      />
-      <div className="bg-white p-2 rounded ml-1">
-          <Subheading bold className="!text-base md:!text-xl">{data.user.displayName}</Subheading>
-          <Flowtext italic className="!text-slate-600 !text-sm md:!text-sm -mt-1">@{data.user.registryName}</Flowtext>
+  const format = (date: Date) => {
+    var d = new Date(date);
+    return d.getDay() + "." + d.getMonth() + "." + d.getFullYear();
+  };
+
+  return !loading ? (
+    <Container className="relative h-screen m-12" sectionMarker>
+      <div className="flex lg:flex-row flex-col bg-white rounded lg:items-center items-start lg:justify-between ">
+        <div className="place-items-center flex flex-row">
+          <Image
+            src={defaultPfp.src}
+            alt=""
+            height={80}
+            width={80}
+            className="rounded-full ring-4 ring-white"
+          />
+          <div className="bg-white p-2 rounded ml-1">
+            <Subheading bold className="!text-base md:!text-xl">
+              {data.user.displayName}
+            </Subheading>
+            <Flowtext
+              italic
+              className="!text-slate-600 !text-sm md:!text-sm -mt-1"
+            >
+              @{data.user.registryName}
+            </Flowtext>
+          </div>
+        </div>
+        <div className="m-4 w-full lg:w-auto">
+          <Flowtext italic center className="!text-slate-600 w-full lg:w-auto">
+            {format(data.begin)} <b>-</b> {format(data.end)}
+          </Flowtext>
+        </div>
       </div>
-      </div>
-      <div className=""><Flowtext italic className="!text-slate-600">from: {data.begin.toString()} to: {data.end.toString()}</Flowtext></div>
-    </div>
-  </Container> : <Loading />
+    </Container>
+  ) : (
+    <Loading />
   );
 };
 
