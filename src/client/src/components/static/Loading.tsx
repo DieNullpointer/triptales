e47443d998dfa491a3dafb3d50edad4911874a61
SCH22538 <SCH22538@spengergasse.at>
2023-11-23 12:07:23 +0100

    feat: fixed loading icon

--- src/client/src/components/static/Loading.tsx
+++ src/client/src/components/static/Loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@material-tailwind/react";
 import React from "react";
 
 const Loading: React.FC<{}> = () => {
-    return <div className="w-full h-full flex items-center justify-center">
+    return <div className="w-full min-h-screen flex items-center justify-center">
         <Spinner className="h-14 w-14" color="green" />
     </div>
 }
