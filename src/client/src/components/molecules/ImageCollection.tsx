6fc67d8c3efd37637a39d8801be765c3d1e2c1ab
SCH22538 <SCH22538@spengergasse.at>
2023-11-23 14:42:49 +0100

    feat: added test image at imageCollection

--- src/client/src/components/molecules/ImageCollection.tsx
+++ src/client/src/components/molecules/ImageCollection.tsx
@@ -1,16 +1,24 @@
+import defaultBanner from "@/resources/default_bannerpic.jpg";
 import Image from "../atoms/Image";
 
 export interface Props {
-    images: [{image: string}];
-    className?: string;
+  images: [{ image: string }];
+  className?: string;
 }
 
-const ImageCollection: React.FC<Props> = ({images, className}) => {
-    return <div className="flex flex-row">
-        {
-            images.map((image) => <Image src={image.image} className="h-48 w-auto" />)
-        }
-    </div>;
-}
+const ImageCollection: React.FC<Props> = ({ images, className }) => {
+  return (
+    <div className="flex flex-row">
+      {images.map((image) => (
+        <Image
+          src={defaultBanner.src}
+          onClick={() => console.log("test")}
+          className="h-full hover:opacity-80 hover:cursor-pointer"
+          wrapper="h-42 max-w-xs overflow-hidden items-center flex rounded"
+        />
+      ))}
+    </div>
+  );
+};
 
 export default ImageCollection;
