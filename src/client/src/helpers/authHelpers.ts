47c6340215c7f627df36d6da14126bbefa105d15
SCH22538 <SCH22538@spengergasse.at>
2023-11-29 15:24:24 +0100

    feat: added redirect from login

--- src/client/src/helpers/authHelpers.ts
+++ src/client/src/helpers/authHelpers.ts
@@ -1,12 +1,30 @@
-import useLocalStorage from "uselocalstoragenextjs";
 import { useRouter } from "next/router";
+import axios from "axios";
 
-export function getBearerToken() {
-    const { value } = useLocalStorage({ name: "bearerToken" });
-    return value;
+axios.defaults.baseURL = "https://localhost:7174/api";
+
+export function getCookie() {
+    return true;
 }
 
 export function isLayoutLessPage() {
-    const router = useRouter()
-    return router.pathname.includes('/landingpage') && !router.pathname.includes('/user') 
+  const router = useRouter();
+  return (
+    router.pathname.includes("/landingpage") &&
+    !router.pathname.includes("/user")
+  );
+}
+
+export async function login(credentials: {
+  registryName: string;
+  password: string;
+}) {
+  const response = await axios.post("/user/login", credentials);
+
+  const resObj = {
+    status: response.status,
+    success: response.status === 200,
+    data: response.data,
+  };
+  return resObj;
 }
--- src/client/src/pages/landingpage/login.tsx
+++ src/client/src/pages/landingpage/login.tsx
@@ -1,47 +1,89 @@
 import Button from "@/components/atoms/Button";
 import Input from "@/components/atoms/Input";
 import Image from "@/components/atoms/Image";
-import loginImage from "@/resources/bild.webp"
+import loginImage from "@/resources/bild.webp";
+import { useState } from "react";
+import { Subheading } from "@/components/atoms/Text";
+import { login } from "@/helpers/authHelpers";
+import { useRouter } from "next/router";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = () => {
+    login({ registryName: username, password });
+    router.push("/");
+  };
+
   return (
-  <div className="flex justify-center items-center">
-    <div className="basis-3/4">
-      <Image src={loginImage.src} alt={""} className="w-full h-screen" />
-    </div>
-    <div className="basis-1/4">
-      <div className="m-4 space-y-4">
-        <h2 className="text-2xl">Loginpage</h2>
-        <Input
-          label="Username"
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
+    <div className="flex justify-center items-center">
+      <div className="basis-3/4">
+        <Image src={loginImage.src} alt={""} className="w-full h-screen" />
+      </div>
+      <div className="basis-1/4">
+        <div className="m-4 space-y-4">
+          <Subheading center uppercase wide>
+            Loginpage
+          </Subheading>
+          <form
+            className="space-y-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <Input
+              label="Username"
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              }
+              value={username}
+              onChange={(val) => setUsername(val)}
+            />
+            <Input
+              type="password"
+              label="Password"
+              value={password}
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M15.75 1.5a6.75 6.75 0 00-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 00-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 00.75-.75v-1.5h1.5A.75.75 0 009 19.5V18h1.5a.75.75 0 00.53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1015.75 1.5zm0 3a.75.75 0 000 1.5A2.25 2.25 0 0118 8.25a.75.75 0 001.5 0 3.75 3.75 0 00-3.75-3.75z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              }
+              onChange={(val) => setPassword(val)}
+            />
+            <Button
+              type="submit"
+              className="text-white w-full"
+              onClick={handleSubmit}
             >
-              <path
-                fillRule="evenodd"
-                d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                clipRule="evenodd"
-              />
-            </svg>
-          }
-        />
-        <Input
-          type="password"
-          bottomText="Must include at least 8 characters"
-          label="Password"
-        />
-        <Button
-          className="text-white w-full"
-        >
-          Login
-        </Button>
+              Login
+            </Button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-
   );
 }
