d8fe31290a14344b1b221025426de8d9056da2a6
SCH22538 <SCH22538@spengergasse.at>
2024-02-02 11:06:06 +0100

    fix: fixed inifinite loading error

--- src/client/src/middleware/middleware.ts
+++ src/client/src/middleware/middleware.ts
@@ -2,7 +2,7 @@ import { User } from "@/types/types";
 import axios from "axios";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url, {credentials: 'include'}).then((res) => res.json());
 
 export function getUser(guid: string) {
   const { data, error, isLoading } = useSWR(
@@ -37,6 +37,19 @@ export function getUserByRegistry(registryName: string): {
       ? "https://localhost:7174/" + data?.profile
       : undefined,
     banner: data?.banner ? "https://localhost:7174/" + data?.banner : undefined,
+    error,
+    isLoading,
+  };
+}
+
+export function getSelf() {
+  const { data, error, isLoading } = useSWR(
+    `https://localhost:7174/api/User/me`,
+    fetcher
+  );
+
+  return {
+    data,
     error,
     isLoading,
   };
--- src/client/src/pages/settings.tsx
+++ src/client/src/pages/settings.tsx
@@ -1,108 +1,66 @@
 import Input from "@/components/atoms/Input";
 import Button from "@/components/atoms/Button";
-import {changeUser, getAuthorizedAll} from "@/helpers/authHelpers"
+import { changeUser, getAuthorizedAll } from "@/helpers/authHelpers";
 import { useEffect, useState } from "react";
 import Loading from "@/components/static/Loading";
-
-import Image from "@/components/atoms/Image";
-import { Flowtext, Heading, Subheading } from "@/components/atoms/Text";
-import Spacing from "@/components/atoms/Spacing";
-import Container from "@/components/atoms/Container";
-import Grid from "@/components/atoms/Grid";
+import { getSelf } from "@/middleware/middleware";
 
 export default function Settings() {
+  const {data, error, isLoading} = getSelf();
+  
   const [user, setUser] = useState<any>();
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [description, setDescription] = useState("");
   const [email, setEmail] = useState("");
   const [origin, setOrigin] = useState("");
   const [favDestination, setFavDestination] = useState("");
 
-  const init = async () => {
-    try {
-      const authorized = await getAuthorizedAll();
-      setUser(authorized)
-      setPassword(user.password);
-      setUsername(user.displayName);
-      setDescription(user.description);
-      setEmail(user.email);
-      setOrigin(user.origin);
-      setFavDestination(user.favDestination);
-    } catch (error: any) {
-      console.error(error.message);
-    }
+  useEffect(() => {
+    setUsername(data?.username);
+    setDescription(data?.description);
+    setEmail(data?.email);
+    setOrigin(data?.origin);
+    setFavDestination(data?.favDestination);
+  }, [data]);
+
+  const handleSubmit = async () => {
+    // CHANGESUBMIT HERE (sry had to delete)
   };
- 
-  init();
 
-
-  
-const handleSubmit = async () => {
-  const response: any = await changeUser({ 
-    registryName: user.username,
-    displayName: username,
-    password: password,
-    email: email,
-    description: description,
-    origin: origin,
-    favDestination: favDestination
-});
-  }; 
-
-   
-
-return user ? (
-  <div className="md:basis-1/4 w-full">
+  return data ? (
+    <div className="md:basis-1/4 w-full">
       <div className="m-4 space-y-4">
-            <Input
-              label="Username"             
-              value={username}
-              onChange={(val) => setUsername(val)}
-            />
-            <Input
-              type="password"
-              label="Password"
-              value={password}              
-              onChange={(val) => setPassword(val)}
-            />
-            <Input
-              label="Description"             
-              value={description}
-              onChange={(val) => setDescription(val)}
-            />
-            <Input
-              label="email"             
-              value={email}
-              onChange={(val) => setEmail(val)}
-            />
-            <Input
-              label="Origin"             
-              value={origin}
-              onChange={(val) => setOrigin(val)}
-            />
-            <Input
-              label="Favorite Destination"             
-              value={favDestination}
-              onChange={(val) => setFavDestination(val)}
-            />
-            <Button
-              type="submit"
-              className="text-white w-full"
-              onClick={handleSubmit}
-            >
-              Save Changes
-            </Button>
+        <Input
+          label="Username"
+          value={username}
+          onChange={(val) => setUsername(val)}
+        />
+        <Input
+          label="Description"
+          value={description}
+          onChange={(val) => setDescription(val)}
+        />
+        <Input label="email" value={email} onChange={(val) => setEmail(val)} />
+        <Input
+          label="Origin"
+          value={origin}
+          onChange={(val) => setOrigin(val)}
+        />
+        <Input
+          label="Favorite Destination"
+          value={favDestination}
+          onChange={(val) => setFavDestination(val)}
+        />
+        <Button
+          type="submit"
+          className="text-white w-full"
+          onClick={handleSubmit}
+        >
+          Save Changes
+        </Button>
       </div>
-  </div>
-
-
-
-
-
-        
-
-    ) : (
-        <Loading />
-    );
+    </div>
+  ) : (
+    <Loading />
+  );
 }
