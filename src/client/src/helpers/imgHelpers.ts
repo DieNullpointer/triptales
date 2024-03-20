e9f3ec168e96cb551b6db278809989d28bfd03c2
SCH22538 <SCH22538@spengergasse.at>
2024-03-20 13:25:34 +0100

    fix: fixed image upload at /settings

--- src/client/src/helpers/imgHelpers.ts
+++ src/client/src/helpers/imgHelpers.ts
@@ -20,3 +20,15 @@ export async function getRandom(): Promise<{
     throw error;
   }
 }
+
+export function dataUrlToFile (dataurl: string, filename: string) {
+  var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)![1],
+        bstr = atob(arr[arr.length - 1]), 
+        n = bstr.length, 
+        u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+};
