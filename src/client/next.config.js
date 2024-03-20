3dea7aef9376251547099a015a590e34d9acd854
SCH22538 <SCH22538@spengergasse.at>
2024-03-20 13:35:29 +0100

    fix: fixed eslint warnings and got build to work

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  distDir: 'build',
   eslint: {
     ignoreDuringBuilds: true
-  },
-  assetPrefix: './'
+  }
 }
 
 module.exports = nextConfig
