6101c1e39a7c284046d6a25b7c7c225cc411aeb6
SCH22538 <SCH22538@spengergasse.at>
2024-03-20 16:15:59 +0100

    fix: fixed username validation

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: 'build',
   eslint: {
     ignoreDuringBuilds: true
-  },
-  assetPrefix: './'
+  }
 }
 
 module.exports = nextConfig
