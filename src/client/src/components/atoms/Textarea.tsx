d6a7fd2478fc13f93380ab6ada70198a7358e52a
SCH22538 <SCH22538@spengergasse.at>
2024-03-21 13:11:08 +0100

    feat: progress on create post

--- src/client/src/components/atoms/Textarea.tsx
+++
@@ -1,9 +0,0 @@
-import { Textarea } from "@material-tailwind/react";
- 
-export function TextareaVariants() {
-  return (
-    <div className="flex w-96 flex-col gap-6">
-        <Textarea label="" />
-    </div>
-  );
-}
